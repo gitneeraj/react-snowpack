59ac316b6f9f04c52620ade47b2cbc2b70741d3b
Neeraj <neeraj.a4353@gmail.com>
2021-04-17 18:35:08 +0530

    initial commit

--- build/App.js
+++ build/App.js
@@ -1,5 +1,8 @@
-import React from "https://cdn.skypack.dev/react";
+import React from "./_snowpack/pkg/react.js";
 const App = () => {
-  return /* @__PURE__ */ React.createElement("div", null, "Hello from react snowpack");
+  return /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("img", {
+    src: "images/sessions/dwm.png",
+    alt: "dwm"
+  }));
 };
 export default App;
